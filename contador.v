c958f39fcb8f475a681d835610e8eb50f7200fa0
Matheus <matheusamg@gmail.com>
2025-04-22 18:21:29 -0300

    Primeiro teste

--- contador.v
+++ contador.v
@@ -1,3 +1,8 @@
+// Se acrescer soma 1
+// Se decrescer diminui 1
+// se acressecer e decrescer mesmo
+// Load = 0x6A
+
 module Contador (
     input  wire clk,
     input  wire rst_n,
@@ -8,6 +13,29 @@ module Contador (
     output reg [7:0] saida
 );
 
-// Insira seu código aqui
+
+wire [7:0] carga;
+assign carga = 8'h6A;
+
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        saida <= carga;
+    end
+    else begin
+        if (saida == 8'hFF)begin
+            saida <= carga;
+        end
+        else if ((acrescer && decrecer) || (~acrescer && ~decrecer))begin // 11 ou 00
+            saida <= saida;
+        end
+        else if (acrescer) begin
+            saida <= saida + 1;
+        end
+        else if (decrecer) begin
+            saida <= saida -1;
+        end
+    end
+end
 
 endmodule
